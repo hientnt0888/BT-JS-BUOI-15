04afba0cd8b91e437054ca172ccb9a3cfed7f92f
Hien <hientnt0999@gmail.com>
2023-07-26 23:08:40 +0700

    update

--- js/tinhTienCap.js
+++ js/tinhTienCap.js
@@ -1,4 +1,4 @@
-function loaiKhachHang() {
+function anSoKetNoi() {
     var KH = document.getElementById("loaiKH").value;
     var soKetNoi = document.getElementById("soKNoi");
     if (KH == 1) {
@@ -9,3 +9,38 @@ function loaiKhachHang() {
 }
 
 
+function tinhTienCap() {
+    var maKhachHang = document.getElementById("maKH").value;
+    var loaiKhachHang = document.getElementById("loaiKH").value;
+    var soKetNoi = document.getElementById("soKNoi").value;
+    var soKenhCC = document.getElementById("kenhCCap").value;
+
+    var tienDan = tienCapDan(soKenhCC);
+    var tienDN = tienCapDN(soKenhCC, soKetNoi);
+    phanLoaiKhachHang(loaiKhachHang, maKhachHang, tienDan, tienDN)
+}
+document.getElementById("btnTienCap").onclick = tinhTienCap;
+
+function tienCapDan(soKenhh) {
+    tienDan = soKenhh * 7.5 + 25;
+
+    return tienDan;
+}
+
+function tienCapDN(soKenh, soKN) {
+    console.log("first", soKenh)
+    tienDN = 0
+    if (soKN <= 10 && soKenh > 0) {
+        tienDN = soKenh *50 + 15 + 7.5 * soKN;
+    } else {
+        tienDN = soKenh * 50 + 90 + (soKN - 10)* 5;
+    }
+    return tienDN;
+}
+
+function phanLoaiKhachHang(loai, maSo, tienCDan, tienCDN) {
+    var tongTienCap = 0;
+    loai == 1 ? tongTienCap = tienCDan : tongTienCap = tienCDN;
+
+    document.getElementById("txtTienCap").innerHTML = "Mã số khách hàng: " + maSo + "<br> Tiền cáp: " + tongTienCap + " $"
+}
--- js/tinhTienDien.js
+++ js/tinhTienDien.js
@@ -1,25 +1,47 @@
+const kw50 = 50
+const kw100 = 100
+const kw200 = 200
+const kw350 = 350
+
 function tongTienCanNop() {
     var tenNguoiDung = document.getElementById("nguoiDung").value;
     var tongSoKW = document.getElementById("soKW").value;
-    var tongTien = 0;
-    if (tongSoKW <= 50 && tongSoKW > 0) {
-        tongTien = tongSoKW * 500;
-        document.getElementById("txtTongTien").innerHTML = "Hộ " + tenNguoiDung + " nôp: " + tongTien + " VND";
-    } else if (tongSoKW > 50 && tongSoKW <= 100){
-        tongTien = (Number(tongSoKW) - 50) * 650 + 50 * 500;
-        document.getElementById("txtTongTien").innerHTML = "Hộ " + tenNguoiDung + " nôp: " + tongTien + " VND";
-    } else if (tongSoKW > 100 && tongSoKW <= 200){
-        tongTien = (Number(tongSoKW) - 100) * 850 + 50 * 500 + 50 * 650;
-        document.getElementById("txtTongTien").innerHTML = "Hộ " + tenNguoiDung + " nôp: " + tongTien + " VND";
-    }  else if (tongSoKW > 200 && tongSoKW <= 350){
-        tongTien = (Number(tongSoKW) - 200) * 1100 + 50 * 500 + 50 * 650 + 100 * 850;
-        document.getElementById("txtTongTien").innerHTML = "Hộ " + tenNguoiDung + " nôp: " + tongTien + " VND";
-    }   else if (tongSoKW > 350){
-        tongTien = (Number(tongSoKW) - 350) * 1300 + 50 * 500 + 50 * 650 + 100 * 850 + 150 * 1100;
-        document.getElementById("txtTongTien").innerHTML = "Hộ " + tenNguoiDung + " nôp: " + tongTien + " VND";
-    } else{
-        document.getElementById("txtTongTien").innerHTML = "Thông tin nhập sai, vui lòng nhập lại chính xác!!";
+    var tienCanNop = 0
+    if (tongSoKW > 0 && tongSoKW <= kw50) {
+        tienCanNop = tien50KWDau(tongSoKW)
+    } else if (tongSoKW > kw50 && tongSoKW <= kw100) {
+        tienCanNop = tien50KWKe(tongSoKW, tien50KWDau(kw50))
+    } else if (tongSoKW > kw100 && tongSoKW <= kw200) {
+        tienCanNop = tien100KWKe(tongSoKW, tien50KWKe(kw100, tien50KWDau(kw50)))
+    } else if (tongSoKW > kw200 && tongSoKW <= kw350) {
+        tienCanNop = tien150KWKe(tongSoKW, tien100KWKe(kw200, tien50KWKe(kw100, tien50KWDau(kw50))))
+    } else if (tongSoKW > kw350) {
+        tienCanNop = tienKWKhac(tongSoKW, tien150KWKe(kw350, tien100KWKe(kw200, tien50KWKe(kw100, tien50KWDau(kw50)))))
     }
+
+    document.getElementById("txtTongTien").innerHTML = "Hộ " + tenNguoiDung + " nôp: " + tienCanNop.toLocaleString() + " VND";
 }
 document.getElementById("btnTongTien").onclick = tongTienCanNop;
 
+function tien50KWDau(tongKW) {
+    var tongTienDien = tongKW * 500
+    return tongTienDien
+}
+function tien50KWKe(tongKW, tien50KW) {
+    var tongTienDien = (Number(tongKW) - 50) * 650 + Number(tien50KW)
+    return tongTienDien
+}
+function tien100KWKe(tongKW, tien100KW) {
+    var tongTienDien = (Number(tongKW) - 100) * 850 + Number(tien100KW)
+    return tongTienDien
+}
+function tien150KWKe(tongKW, tien200KW) {
+    console.log("🚀 ~ file: demo.js:38 ~ tien150KWKe ~ tongKW:", tongKW)
+    var tongTienDien = (Number(tongKW) - 200) * 1100 + Number(tien200KW)
+    return tongTienDien
+}
+function tienKWKhac(tongKW, tien350KW) {
+    console.log("🚀 ~ file: demo.js:43 ~ tienKWKhac ~ tongKW:", tongKW)
+    var tongTienDien = (Number(tongKW) - 350) * 1300 + Number(tien350KW)
+    return tongTienDien
+}
